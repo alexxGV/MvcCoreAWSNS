3195a3636e10a9f5ada1c2e822a4660c599d81a6
Alex <alexx.garci@gmail.com>
2021-05-21 11:47:01 +0200

    Add project files.

---
+++ MvcCoreAWSNS/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MvcCoreAWSNS.Services;
+
+namespace MvcCoreAWSNS.Controllers
+{
+    public class HomeController : Controller
+    {
+        private ServiceSNS ServiceSNS;
+        public HomeController(ServiceSNS service)
+        {
+            this.ServiceSNS = service;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult EnviarMensaje()
+        {
+            return View();
+        }
+        
+        [HttpPost]
+        public IActionResult EnviarMensaje(String mensaje, String asunto)
+        {
+            ViewData["RESPUESTA"] = this.ServiceSNS.SeedMessage(mensaje, asunto);
+            return View();
+        }
+    }
+}
---
+++ MvcCoreAWSNS/Services/ServiceSNS.cs
@@ -0,0 +1,45 @@
+﻿using Amazon.SimpleNotificationService;
+using Amazon.SimpleNotificationService.Model;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCoreAWSNS.Services
+{
+    public class ServiceSNS
+    {
+        IConfiguration Configuration;
+        public ServiceSNS(IConfiguration configuration)
+        {
+            this.Configuration = configuration;
+        }
+
+        public String SeedMessage(String message, String subject)
+        {
+            string topicArn = this.Configuration["SNS:TopicArn"];
+
+            var client = new AmazonSimpleNotificationServiceClient(region: Amazon.RegionEndpoint.USEast1);
+
+            var request = new PublishRequest
+            {
+                Subject = subject,
+                Message = message,
+                TopicArn = topicArn
+            };
+
+            try
+            {
+                var response = client.PublishAsync(request);
+
+                return ("Mensaje:  " + subject + ", enviado correctamente.");
+            }
+            catch (Exception ex)
+            {
+                return ("ERROR al publicar mensaje: "+ ex.Message);
+            }
+
+        }
+    }
+}
